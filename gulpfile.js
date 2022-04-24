fa605758b0cf02ae1578758e521298d192c18e50
Aurinkoinen87 <imax449@yandex.ru>
2022-04-24 05:17:58 +0300

    1

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+import gulp from 'gulp'
+import { path } from './gulp/config/path.js'
+import { copy } from './gulp/tasks/copy.js'
+import { reset } from './gulp/tasks/reset.js'
+import { html } from './gulp/tasks/html.js'
+
+global.app = {
+  gulp: gulp,
+  path: path
+}
+
+
+function watcher(){
+  gulp.watch(path.watch.files, copy) 
+  gulp.watch(path.watch.html, html) 
+}
+
+
+const mainTasks = gulp.parallel(copy, html)
+
+const dev = gulp.series(reset, mainTasks, watcher)
+
+gulp.task('default', dev)
